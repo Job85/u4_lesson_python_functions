56eff13c094348d133a02e45dd0a6e0ab570958a
Job85 <julianjernigan@ymail.com>
2022-04-29 11:55:13 -0400

    completed lesson

--- main.py
+++ main.py
@@ -1,9 +1,23 @@
 #  Create a function called "return_name" that returns your name as a string.
-
+def return_name():
+    return 'Julian'
 # Create a function called "power" that accepts a parameter of num and returns that num to the second power.
 
+
+def power(num):
+    return(num**2)
 # Create a function called "total_sum" that accepts two parameters num1 and num2 and return the sum of those two parameters
 
+
+def total_sum(num1, num2):
+    return(num1+num2)
 # Create a function called "remainder" that accepts two parameters and returns the remainder of num1 and num2, HINT: Use the modulus operator.
 
+
+def remainder(num1, num2):
+    return(num1 % num2)
 # Create a function called "reassign_sum" that accepts one parameter and returns that parameter and assigns it to plus 5
+
+
+def reassign_sum(num):
+    return(num+5)
